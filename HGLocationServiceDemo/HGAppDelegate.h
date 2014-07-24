f098353c9586da78b16e5bbe01753f6c16dac8c0
wangrui <wangrui@dxyer.com>
2014-07-24 10:09:24 +0800

    Initial Commit

---
+++ HGLocationServiceDemo/HGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HGAppDelegate.h
+//  HGLocationServiceDemo
+//
+//  Created by HamGuy on 7/24/14.
+//  Copyright (c) 2014 ___DXY___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HGLocationServiceDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HGLocationServiceDemo
+//
+//  Created by HamGuy on 7/24/14.
+//  Copyright (c) 2014 ___DXY___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HGAppDelegate class]));
+    }
+}
